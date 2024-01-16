fed89fa5ccd70fc4219b0f15216cfff3dabc82a8
LeartIstrefaj <leartistrefaj02@gmail.com>
2024-01-16 22:27:57 +0100

    Service-Logging created

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
+
+namespace Service_Logging
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var configFile = new ConfigurationBuilder().AddJsonFile("appsettings.json",false,true).Build();
+            
+            var services = new ServiceCollection();
+            services.AddLogging(
+                log => { log.ClearProviders(); log.AddNLog(); }
+                );
+        }
+    }
+}
